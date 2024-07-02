249f2ac573c692e6b2b375812f81752965428f64
Kik87474 <117936162+Kik87474@users.noreply.github.com>
2024-07-02 16:45:54 +0200

    injected code to fetch dynamic data fore forecast

--- src/app.js
+++ src/app.js
@@ -63,20 +63,26 @@ function getForecast(city) {
 }
 
 function displayForecast(response) {
+  console.log(response.data);
+
   let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
   let forecastHtml = "";
 
-  days.forEach(function (day) {
+  response.data.daily.forEach(function (day) {
     forecastHtml =
       forecastHtml +
       `<div class="weather-forecast-day">
-            <div class="weather-forecast-date">${day}</div>
-            <div class="weather-forecast-icon">⛅</div>
+            <div class="weather-forecast-date">Tue</div>
+            <div class="weather-forecast-icon">
+            <img src="${day.condition.icon_url}"/>
+            </div>
             <div class="weather-forecast-temperatures">
               <div class="weather-forecast-temperature">
-                <strong>15°</strong>
+                <strong>${Math.round(day.temperature.maximum)}°</strong>
               </div>
-              <div class="weather-forecast-temperature">9°</div>
+              <div class="weather-forecast-temperature">${Math.round(
+                day.temperature.minimum
+              )}°</div>
             </div>
           </div>`;
   });
